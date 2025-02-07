6aa0e2bde9d224471cac381d35d59f6ac2cd983d
thatguyKacper <kacper.m.wojtowicz@gmail.com>
2025-02-07 20:20:00 +0100

    add logic

---
+++ src/NestedInput.tsx
@@ -0,0 +1,45 @@
+import { useForm, FormProvider, useFormContext } from 'react-hook-form'
+
+import './Form.css'
+
+interface FormData {
+  name: string
+}
+
+const NestedInput = () => {
+  const {
+    register,
+
+    formState: { errors },
+  } = useFormContext<FormData>()
+
+  return (
+    <div>
+      <input
+        {...register('name', { required: 'Name is required' })}
+        type="text"
+        placeholder="Name"
+      />
+
+      <p className="error">{errors.name?.message}</p>
+    </div>
+  )
+}
+
+export default function NestedForm() {
+  const form = useForm<FormData>()
+
+  const { handleSubmit } = form
+
+  const onSubmit = () => {}
+
+  return (
+    <FormProvider {...form}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <NestedInput />
+
+        <button type="submit">Signup</button>
+      </form>
+    </FormProvider>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import NestedInput from './NestedInput.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <NestedInput />
+  </StrictMode>
 )
